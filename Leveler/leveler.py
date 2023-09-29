14347c8c1f87f1b06c6a3f245ca98eabc64abed2
nmavis <nmavis@cisco.com>
2023-09-29 17:05:06 -0400

    Smart guy forgot slash

--- Leveler/leveler.py
+++ Leveler/leveler.py
@@ -172,7 +172,7 @@ class Leveler(commands.Cog):
         #font2 = ImageFont.truetype(fontpath, 22)
         #font3 = ImageFont.truetype(fontpath, 32)
 
-        rsc_font = f"{Path.home()}fonts/Apotek_Black.otf"
+        rsc_font = f"{Path.home()}/fonts/Apotek_Black.otf"
 
         font1 = ImageFont.truetype(rsc_font, 18)
         font2 = ImageFont.truetype(rsc_font, 22)
